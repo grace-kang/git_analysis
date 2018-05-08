08b6da49d26c60a9b9c9da4cfc8e7a6c68e7fefd
Grace Kang <grace@stembolt.com>
2018-05-08 10:41:18 -0700

    adding gem into new repo

---
+++ lib/git_analysis/version.rb
@@ -0,0 +1,3 @@
+module GitAnalysis
+  VERSION = "0.1.0"
+end
---
+++ spec/git_analysis_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe GitAnalysis do
+  it "has a version number" do
+    expect(GitAnalysis::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
