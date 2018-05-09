bdc159eeb6b05427862b4d68df8327a0595f3de7
Grace Kang <grace@stembolt.com>
2018-05-09 09:52:08 -0700

    renamed analyzer -> gitanalyzer

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require_relative 'lib/git_analysis/analyzer'
+require_relative 'lib/git_analysis/gitanalyzer'
 require 'octokit'
 
 # check argument length
@@ -17,7 +17,7 @@ end
 owner = repo.owner
 repo = repo.name.split('.')[0]
 
-git_analyzer = GitAnalysis::Analyzer.new(ENV['TOKEN'], repo, owner)
+git_analyzer = GitAnalysis::GitAnalyzer.new(ENV['TOKEN'], repo, owner)
 git_analyzer.print_basic_info
-git_analyzer.print_num_PRs
-git_analyzer.print_PR_sizes
+git_analyzer.print_num_prs
+git_analyzer.print_pr_sizes
